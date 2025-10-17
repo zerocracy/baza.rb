2f63ad630bd831fdd906b2b44dbb56dc09858945
l3r8yJ <l3r8y@duck.com>
2025-10-17 15:58:03 +0300

    fix(#122): braces

--- lib/baza-rb.rb
+++ lib/baza-rb.rb
@@ -676,7 +676,7 @@ class BazaRb
     attempt = 0
     loop do
       ret = yield
-      if ret.code == 429 || ret.code >= 500 && attempt < @retries
+      if (ret.code == 429 || ret.code >= 500) && attempt < @retries
         attempt += 1
         seconds = 2 * @pause * attempt
         @loog.info("Server seems to be in trouble, will sleep for #{seconds} (attempt no.#{attempt})...")
